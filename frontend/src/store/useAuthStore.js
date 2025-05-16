0dc09bb45f5035e0bbc2a6ebb7467d038bc19548
Alan Vargas <137660545+Alang315@users.noreply.github.com>
2025-05-16 16:53:40 -0600

    Nav-Bar Fixed with localStorage (#4)

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -54,6 +54,7 @@ export const useAuthStore = create((set) => ({
         try {
             await axiosInstance.post("/auth/logout");
             set({ authUser: null });
+            localStorage.clear();
             toast.success("Logged out successfully");
         } catch (error) {
             toast.error(error.response.data.message);
