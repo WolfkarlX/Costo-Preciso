7481003a5e281d8749c76443627bc54b175ed69b
Alan Vargas <137660545+Alang315@users.noreply.github.com>
2025-05-19 13:03:46 -0600

    endpoint GetSpecifiedinredient finished (#5)

--- backend/src/routes/ingredient.route.js
+++ backend/src/routes/ingredient.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createIngredient, deleteIngredient, getIngredients, updateIngredient } from "../controllers/ingredient.controller.js";
+import { createIngredient, deleteIngredient, getIngredients, getSpecificingredient, updateIngredient } from "../controllers/ingredient.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { validateAndFilterUpdates } from "../middleware/util.middleware.js";
 
@@ -8,6 +8,7 @@ const allowedxUpdates = ['name', 'Units', 'unityOfmeasurement', 'totalPrice', 'i
 
 router.post("/create", protectRoute, createIngredient);
 router.get("/ingredients", protectRoute, getIngredients);
+router.get("/ingredient/:id", protectRoute, getSpecificingredient);
 router.delete("/del/:id", protectRoute, deleteIngredient);
 router.post("/updt/:id", protectRoute, validateAndFilterUpdates(allowedxUpdates), updateIngredient);
 
