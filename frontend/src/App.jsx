6a10083e38e0f6dfb06cb94dfb815ab2cec046a9
Karla <kramirez32@ucol.mx>
2025-05-27 15:24:12 -0600

    Fixed conflicts between Google-auth and main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,13 +17,13 @@ import { Toaster } from "react-hot-toast";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const GOOGLE_CLIENT_ID = "489214754720-hrn227tne35st7tetb4mbpn3f90t3c7g.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID =
+  "489214754720-hrn227tne35st7tetb4mbpn3f90t3c7g.apps.googleusercontent.com";
 
 const App = () => {
-
-  const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   const location = useLocation();
-  
+
   useEffect(() => {
     checkAuth();
   }, [location.pathname, checkAuth]);
@@ -38,24 +38,42 @@ const App = () => {
     );
 
   return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <div>
+        {authUser ? <Navbar onNavClick={checkAuth} /> : null}
 
-    <div>
-      {authUser ? <Navbar onNavClick={checkAuth} /> : ""}
-      
-      <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/settings" element={authUser ? <SettingsPage /> : <Navigate to="/login" />} />
-        <Route path="/ingredients" element={authUser ? <IngredientsPage /> : <Navigate to="/login" />} />
-        <Route path="/percentages" element={authUser ? <PercentagesPage /> : <Navigate to="/login" />} />
-        <Route path="/sales" element={authUser ? <SalesPage /> : <Navigate to="/login" />} />
+        <Routes>
+          <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/settings"
+            element={authUser ? <SettingsPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/ingredients"
+            element={authUser ? <IngredientsPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/percentages"
+            element={authUser ? <PercentagesPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/sales"
+            element={authUser ? <SalesPage /> : <Navigate to="/login" />}
+          />
+        </Routes>
 
-      </Routes>
-
-      <Toaster />
-    </div>
-  </GoogleOAuthProvider>
+        <Toaster />
+      </div>
+    </GoogleOAuthProvider>
   );
 };
+
 export default App;
