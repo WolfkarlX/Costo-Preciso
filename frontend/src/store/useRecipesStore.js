1dad9d67f11f66bbe6a61d614c9b78a59f4eaa25
fatimaaa21 <fcorona10@ucol.mx>
2025-05-29 21:49:47 -0600

    CRUD completo en sección de Recetas

--- frontend/src/store/useRecipesStore.js
+++ frontend/src/store/useRecipesStore.js
@@ -4,7 +4,22 @@ import { data } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export const useRecipesStore = create((set, get) => ({
+    ingredients: [],
+    recipes: [],
     isCreating: false,
+    isGetting: false,
+
+    fetchIngredients: async () => {
+        set({ isGetting: true });
+        try {
+            const res = await axiosInstance.get("http://localhost:5001/api/ingredient/ingredients");
+            set({ ingredients: res.data }); // Guarda la lista en el estado
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error al obtener ingredientes.");
+        } finally {
+            set({ isGetting: false });
+        }
+    },
 
     create: async (data) => {
         set({ isCreating: true });
@@ -17,4 +32,43 @@ export const useRecipesStore = create((set, get) => ({
             set({ isCreating: false });
         }
     },
+
+    fetchRecipes: async () => {
+        set({ isGetting: true });
+        try {
+            const res = await axiosInstance.get("http://localhost:5001/api/recipe/recipes");
+            set({ recipes: res.data }); // Guarda la lista en el estado
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error al obtener ingredientes.");
+        } finally {
+            set({ isGetting: false });
+        }
+    },
+
+    updateRecipe: async (id, updatedData) => {
+        try {
+            await axiosInstance.post(`http://localhost:5001/api/recipe/updt/${id}`, updatedData);
+            toast.success("Recipe updated");
+            const recipes = get().recipes.map((ing) =>
+                ing._id === id ? { ...ing, ...updatedData } : ing
+            );
+            set({ recipes });
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error al actualizar receta");
+        }
+    },
+
+    deleteRecipes: async (id) => {
+    try {
+        await axiosInstance.delete(`http://localhost:5001/api/recipe/del/${id}`);
+        
+        const updatedRecipe = get().recipes.filter((ing) => ing._id !== id);
+        set({ recipes: updatedRecipe });
+
+        toast.success("Recipe deleted");
+    } catch (error) {
+        toast.error(error.response?.data?.message || "Error al eliminar receta");
+    }
+},
+
 }));
